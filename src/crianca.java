87e23f176962131a57266872fce18370672dc95a
kleitoncls <kleitoncls@hotmail.com>
2019-03-07 20:33:48 -0300

    Initial commit

---
+++ src/crianca.java
@@ -0,0 +1,12 @@
+public class crianca {
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+    void brincar(){
+        System.out.println("brincando");
+    }
+    void correr(double metros){
+        System.out.println("correndo");
+    }
+}
